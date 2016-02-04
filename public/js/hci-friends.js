497b85b084b6527d715a9e49eed2048a0c9157bb
Kirk Srijongsirikul <srijongsirikul.kirk@gmail.com>
2016-02-04 11:56:41 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,14 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+	$(".person-name").click(function(e) {
+		e.preventDefault();
+		console.log("name clicked!");
+
+		var newName = anagrammedName($(this).text());
+		$(this).text(newName);
+	});
 })
 
 /*
@@ -14,10 +22,10 @@ function initializePage() {
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
-	} 
+	}
 	else if (name == "Ivan Sutherland") {
 		return "Vandal Heist Run";
 	}
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	res.render('index', data);
+		/*'name': 'Fake friend',
+		'imageURL': 'http://lorempixel.com/500/500/people',
+		'description': 'All my friends are imaginary'*/
 };
